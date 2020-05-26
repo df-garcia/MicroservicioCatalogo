8cc46b0b41aa4c27eadb038baeb9e6bae511c68f
Daniel <df.garcia@uniandes.edu.co>
2020-05-25 21:44:10 -0500

    Probando Retrieve

---
+++ MicroservicioCatalogo/src/main/java/com/microservices/catalogue/MicroservicioCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCatalogoApplication.class, args);
+	}
+
+}
